4ef548ea07c98369b589178300cb2a252aae6e84
SFDev <sfdev@decodevs.net>
2025-06-26 16:25:34 -0500

    feat(bot): add dynamic loading for command cogs

--- app/main.py
+++ app/main.py
@@ -9,7 +9,18 @@ intents = discord.Intents.default()
 bot = discord.Client(intents=intents)
 
 
+async def load_cogs():
+    for filename in os.listdir("app/commands"):
+        if filename.endswith(".py"):
+            extension = f"cogs.{filename[:-3]}"
+            try:
+                await bot.load_extension(extension)
+            except Exception as e:
+                print(e)
+
+
 async def start_all():
+    await load_cogs()
     await bot.start(os.getenv("BOT_TOKEN"))
 
 # --- Entry point ---
