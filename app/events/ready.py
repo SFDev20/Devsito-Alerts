d0b91febd546f783f502c47ab75833576e5fc296
SFDev <sfdev@decodevs.net>
2025-06-26 17:36:32 -0500

    feat(ready): add event class for on_ready

---
+++ app/events/ready.py
@@ -0,0 +1,18 @@
+from discord.ext import commands
+
+
+class Ready(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.synced = False
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        if not self.synced:
+            await self.bot.tree.sync()
+            self.synced = True
+        print(f"Bot ready as {self.bot.user}")
+
+
+async def setup(bot):
+    await bot.add_cog(Ready(bot))
